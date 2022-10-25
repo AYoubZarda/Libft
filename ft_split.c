694765cbe722ea6dc966446b519f6f2e07d7b063
AYoubZarda <115242314+AYoubZarda@users.noreply.github.com>
2022-10-26 00:35:56 +0100

    split

--- ft_split.c
+++ ft_split.c
@@ -5,38 +5,79 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: azarda <azarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/23 12:27:21 by azarda            #+#    #+#             */
-/*   Updated: 2022/10/25 18:31:08 by azarda           ###   ########.fr       */
+/*   Created: 2022/10/26 00:02:38 by azarda            #+#    #+#             */
+/*   Updated: 2022/10/26 00:06:55 by azarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char ft_calcul(const char *s, char d)
+char	ft_calcul(const char *s, char d)
 {
-	int i;
-	int cont;
-	
+	int	i;
+	int	cont;
+
 	if (s[0] != d)
 		cont = 1;
 	else
 		cont = 0;
-	while(s[i])
+	i = 0;
+	while (s[i])
 	{
-	if (s[i] && s[i] == d && s[i + 1] != d)
-	cont++;
+		if (s[i] && s[i] == d && s[i + 1] != d && s[i + 1] != '\0')
+			cont++;
 		i++;
 	}
-	 return (cont);  
+	return (cont);
 }
-// char    **ft_split(char const *s, char c)
-// {
-	
 
-	
-// }
-int main()
+void	ft_free_(char **tab)
 {
-char *d = "zarda ayoub ahvg kvj     ";
-printf("%d\n", ft_calcul(d, ' '));
+	int	i;
+
+	i = 0;
+	while (tab[i])
+		free(tab[i++]);
+	free(tab);
+}
+
+char	**ft_my_split(char const *str, char d, int i, int j)
+{
+	char	**tab;
+	int		debut;
+	int		fin;
+
+	if (!str)
+		return (NULL);
+	tab = (char **)malloc(sizeof(char *) * (ft_calcul(str, d) + 1));
+	if (!tab)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		while (str[i] && str[i] == d)
+			i++;
+		debut = i;
+		while (str[i] && str[i] != d)
+			i++;
+		fin = i;
+		if (j < ft_calcul(str, d))
+		{
+			tab[j++] = ft_substr(str, debut, fin - debut);
+			if (!tab[j - 1])
+				return (ft_free_(tab), NULL);
+		}
+	}
+	return (tab[j] = NULL, tab);
+}
+
+char	**ft_split(char const *st, char c)
+{	
+	char	**tab;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	tab = ft_my_split(st, c, i, j);
+	return (tab);
 }
