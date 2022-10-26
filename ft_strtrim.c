59e6c41c2a716f56c71e1aaf876fddb49d6327a8
AYoubZarda <115242314+AYoubZarda@users.noreply.github.com>
2022-10-26 20:55:03 +0100

    hh

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: azarda <azarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/22 19:01:27 by azarda            #+#    #+#             */
-/*   Updated: 2022/10/23 19:06:54 by azarda           ###   ########.fr       */
+/*   Updated: 2022/10/26 20:51:24 by azarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,18 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t i;
-	size_t j;
-	
-	i = 0;
-	
-	if (!s1)
-		return (NULL);
-	if (!set)
-		return ((char*)s1);
+	size_t	i;
+	size_t	j;
 
+	i = 0;
+	if (!s1 || !set)
+		return (0);
 	j = ft_strlen((char *)s1);
-	while(i < ft_strlen((char *)s1) && ft_strchr(set, s1[i]))
+	while (i < j && ft_strchr(set, s1[i]))
 		i++;
-	j--;
-	while(j >= 0 && ft_strchr(set,s1[j]))
+	if (i == j)
+		return (ft_strdup(""));
+	while (j >= 0 && ft_strrchr(set, s1[j]))
 		j--;
 	return (ft_substr(s1, i, (j - i + 1)));
 }
