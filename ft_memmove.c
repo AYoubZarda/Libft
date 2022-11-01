491d2f910aee38c6e18a6fce373c008c2ac682ed
AYoubZarda <115242314+AYoubZarda@users.noreply.github.com>
2022-11-01 17:08:32 +0100

    55

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: azarda <azarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 16:51:08 by azarda            #+#    #+#             */
-/*   Updated: 2022/10/31 19:39:27 by azarda           ###   ########.fr       */
+/*   Updated: 2022/11/01 17:03:02 by azarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 	char	*ds;
 	char	*sr;
 
-	if (dst == NULL && src == NULL)
-		return (NULL);
 	if (dst == src)
 		return (dst);
 ds = (char *)dst;
