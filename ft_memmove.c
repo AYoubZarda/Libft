9d3e56d1cbe3e00dfa943f5d983557d94b912e8e
AYoubZarda <zardaayoub1@gmail.com>
2022-11-07 11:07:03 +0100

    99

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: azarda <azarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/12 16:51:08 by azarda            #+#    #+#             */
-/*   Updated: 2022/11/07 10:28:42 by azarda           ###   ########.fr       */
+/*   Updated: 2022/11/07 10:56:49 by azarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char	*ds;
-	char	*sr;
+	unsigned char	*ds;
+	unsigned char	*sr;
 
 	if (dst == src)
 		return (dst);
-	ds = (char *)dst;
-	sr = (char *)src;
+	ds = (unsigned char *)dst;
+	sr = (unsigned char *)src;
 	if (sr > ds)
 		while (len--)
-	*(ds++) = *(sr++);
+			*(ds++) = *(sr++);
 	else
 		while (len--)
-	*(ds + len) = *(sr + len);
+			*(ds + len) = *(sr + len);
 	return (dst);
 }
